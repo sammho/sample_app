80f5e9aacf5aa6ff7b89d417cf848fc47cdffa2b
Sam Ho <samho@peoplematch.me>
2011-10-11 00:54:25 -0700

    user signup complete

--- config/application.rb
+++ config/application.rb
@@ -1,5 +1,12 @@
 require File.expand_path('../boot', __FILE__)
 
+require 'rails/all'
+
+# If you have a Gemfile, require the gems listed there, including any gems
+# you've limited to :test, :development, or :production.
+Bundler.require(:default, Rails.env) if defined?(Bundler)
+
+
 # Pick the frameworks you want:
 require "active_record/railtie"
 require "action_controller/railtie"
